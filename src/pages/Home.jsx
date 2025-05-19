8290333b3bc378aa859266bd03444832f206ce15
uzair-mirza <mirzauzair558@gmail.com>
2025-05-20 00:34:15 +0500

    Refactor CSS padding in sections and add new styles for sidebar links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,59 +1,58 @@
 import React from 'react'
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-
-// import required modules
-import { Autoplay, Pagination } from 'swiper/modules';
-import Cards from '../components/UI/Cards';
+import { NavLink } from 'react-router-dom'
 
 function Home() {
   return (
     <>
-    {/* <section>
-        <div className="container">
+    <section className=''>
+        <div className="container-fluid">
             <div className="row">
-                <div className="col-lg-6">
-                    <div className='banner-heading'>
-                        <h1>Your Feet Deserve The Best</h1>
-                        <p>Your feet deserve the best and we’re here to help you with our shoes. Your feet deserve the best and we’re here to help you with our shoes.</p>
-                        <div>
-                        <button className="navbar-login">Shop Now</button>
+                <div className="col-lg-3">
+                    <div className='bg-white links_sec'>
+                        <div className=''>
+                            <h3><img className='me-2' src="/images/quick.png" alt="" />Quick Links</h3>
+                        </div>
+                        <div className='main_side_bar'>
+                            <ul>
+                                <li><NavLink to="/" className="text-decoration-none"><img src="/images/live_pic.png" alt="" className='me-2' />Live Now</NavLink></li>
+                                <li><NavLink to="/" className="text-decoration-none"><img src="/images/ass_pic.png" alt="" className='me-2' />Assignment</NavLink></li>
+                                <li><NavLink to="/" className="text-decoration-none"><img src="/images/predict_pic.png" alt="" className='me-2' />Predictions</NavLink></li>
+                                <li><NavLink to="/" className="text-decoration-none"><img src="/images/team_pic.png" alt="" className='me-2' />Team</NavLink></li>
+                                <li><NavLink to="/" className="text-decoration-none"><img src="/images/top_pic.png" alt="" className='me-2' />Top Championships</NavLink></li>
+                                <li><NavLink to="/" className="text-decoration-none"><img src="/images/countries_pic.png" alt="" className='me-2' />Top Countries</NavLink></li>
+                            </ul>
                         </div>
                     </div>
+                    {/* */}
+                    <div className='bg-white links_sec mt-3'>
+                        <div className='main_side_bar'>
+                            <ul>
+                                <li><NavLink to="/" className="text-decoration-none">Get your Fan ID Card</NavLink></li>
+                                <li className='mb-0 text-end'><NavLink to="/" className="text-decoration-none"><img src="/images/fanid_pic.png" alt=""  /></NavLink></li>
+                            </ul>
+                        </div>
+                    </div>
+                    {/*  */}
+                    {/* */}
+                    <div className='bg-white links_sec mt-3'>
+                        <div className='main_side_bar'>
+                            <ul>
+                                <li><NavLink to="/" className="text-decoration-none">Vote your Favorite Player</NavLink></li>
+                                <li className='mb-0 text-end'><NavLink to="/" className="text-decoration-none"><img src="/images/fav_pl_pic.png" alt=""  /></NavLink></li>
+                            </ul>
+                        </div>
+                    </div>
+                    {/*  */}
                 </div>
                 <div className="col-lg-6">
-                    <div>
-                        <Swiper 
-                            pagination={true} 
-                            loop 
-                            modules={[Pagination, Autoplay]}
-                            autoplay={{
-                                delay: 2500,
-                                disableOnInteraction: false,
-                              }} 
-                            className="mySwiper">
-                            <SwiperSlide><img src="/images/shoe_image.png" alt="" /></SwiperSlide>
-                            <SwiperSlide><img src="/images/shoe_image.png" alt="" /></SwiperSlide>
-                            <SwiperSlide><img src="/images/shoe_image.png" alt="" /></SwiperSlide>
-                            <SwiperSlide><img src="/images/shoe_image.png" alt="" /></SwiperSlide>
-                            <SwiperSlide><img src="/images/shoe_image.png" alt="" /></SwiperSlide>
-                        </Swiper>
-                    </div>
+
+                </div>
+                <div className="col-lg-3">
+
                 </div>
             </div>
         </div>
     </section>
-    <section>
-        <div className="container">
-            <div className="row">
-                <Cards />
-            </div>
-        </div>
-    </section> */}
     </>
   )
 }
